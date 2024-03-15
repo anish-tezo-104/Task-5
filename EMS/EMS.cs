6b2086f27de4f5d1fa6d4b97e2bcc44ddc01a193
anish-tezo-104 <anish.m@Technovert.com>
2024-03-15 13:26:10 +0530

    added separate constants class

--- EMS/EMS.cs
+++ EMS/EMS.cs
@@ -12,28 +12,22 @@ public partial class EMS
 {
     private readonly static IEmployeeDAL _employeeDal;
     private readonly static IEmployeeBAL _employeeBal;
-    internal static JSONUtils jsonUtils;
+    private static JSONUtils jsonUtils;
     private static readonly ILogger _logger;
+    private static readonly string employeeFilePath = GetIConfiguration()["environmentVariables:EMPLOYEE__JSON__PATH"];
 
     public static partial Employee GetEmployeeDataFromUser();
-
     public static partial void PrintEmployeesDetails(List<EmployeeDetails> employees);
-
     public static partial IConfiguration GetIConfiguration();
-
     public static partial EmployeeFilters? GetEmployeeFiltersFromConsole();
-
     public static partial EmployeeFilters? GetSearchKeywordFromConsole();
-
     public static partial Employee GetUpdatedDataFromUser();
-
-    internal static string EmployeeFilePath = GetIConfiguration()["environmentVariables:EMPLOYEE__JSON__PATH"];
 
     static EMS()
     {
         jsonUtils = new JSONUtils();
         _logger = new ConsoleLogger();
-        _employeeDal = new EmployeeDAL(_logger, jsonUtils, EmployeeFilePath);
+        _employeeDal = new EmployeeDAL(_logger, jsonUtils, employeeFilePath);
         _employeeBal = new EmployeeBAL(_logger, _employeeDal);
     }
 
@@ -60,17 +54,17 @@ public partial class EMS
         Employee employee = GetEmployeeDataFromUser();
         if (employee == null)
         {
-            _logger.LogError("Error retrieving employee data from console.");
+            _logger.LogError(Constants.GettingDataFromConsoleError);
             return;
         }
         bool status = _employeeBal.AddEmployee(employee);
         if (status)
         {
-            _logger.LogSuccess("Employee added successfully.");
+            _logger.LogSuccess(Constants.AddEmployeeSuccess);
         }
         else
         {
-            _logger.LogError("Some error occur while adding employee. Please try again!");
+            _logger.LogError(Constants.AddEmployeeError);
         }
     }
 
@@ -81,33 +75,28 @@ public partial class EMS
             List<EmployeeDetails> employees = _employeeBal.GetAll();
             if (employees != null && employees.Count > 0)
             {
-                _logger.LogSuccess($"Found {employees.Count} employees : \n");
+                _logger.LogSuccess($"{employees.Count} {Constants.RetrieveAllEmployeesSuccess}");
                 PrintEmployeesDetails(employees);
             }
         }
-        catch (Exception ex)
+        catch (Exception)
         {
 
-            _logger.LogError($"Error while retrieving employees: {ex.Message}");
+            _logger.LogError(Constants.RetrieveAllEmployeesError);
         }
     }
 
     public static void DeleteEmployee(string empNo)
     {
-        if (empNo == null)
-        {
-            _logger.LogError("Please provide the employee number.");
-            return;
-        }
         bool status = _employeeBal.DeleteEmployees(empNo);
         if (status)
         {
-            _logger.LogSuccess("Employee deleted successfully.");
+            _logger.LogSuccess(Constants.DeleteEmployeeSuccess);
 
         }
         else
         {
-            _logger.LogError("Some error occur while deleting employee. Please try again!");
+            _logger.LogError(Constants.DeleteEmployeeError);
         }
     }
 
@@ -121,13 +110,13 @@ public partial class EMS
         }
         catch (Exception)
         {
-            _logger.LogError("Error occurred while searching for employees. Please try again!");
+            _logger.LogError(Constants.SearchEmployeeError);
             return;
         }
 
         if (employees == null || employees.Count == 0)
         {
-            _logger.LogSuccess("No employee found with the provided employee number.");
+            _logger.LogSuccess(Constants.EmpNoNotFound);
             return;
         }
 
@@ -140,16 +129,16 @@ public partial class EMS
             bool status = _employeeBal.UpdateEmployee(empNo, updatedEmployee);
             if (status)
             {
-                _logger.LogSuccess("Employee data updated successfully.");
+                _logger.LogSuccess(Constants.UpdateEmployeeSuccess);
             }
             else
             {
-                _logger.LogError("Error occurred while updating employee data.");
+                _logger.LogError(Constants.UpdateEmployeeError);
             }
         }
         catch (Exception)
         {
-            _logger.LogError("Error occurred while updating employee data.");
+            _logger.LogError(Constants.UpdateEmployeeError);
         }
     }
 
@@ -158,7 +147,7 @@ public partial class EMS
         EmployeeFilters? keyword = GetSearchKeywordFromConsole();
         if (keyword == null)
         {
-            _logger.LogError("No keyword provided for search.");
+            _logger.LogError(Constants.SearchNoKeywordError);
             return;
         }
         try
@@ -166,13 +155,13 @@ public partial class EMS
             List<EmployeeDetails> employees = _employeeBal.SearchEmployees(keyword);
             if (employees != null && employees.Count > 0)
             {
-                _logger.LogSuccess($"Found {employees.Count} employees.");
+                _logger.LogSuccess($"{employees.Count} {Constants.SearchEmployeeSuccess}");
                 PrintEmployeesDetails(employees);
             }
         }
-        catch (Exception ex)
+        catch (Exception)
         {
-            _logger.LogError($"Error while searching employees: {ex.Message}");
+            _logger.LogError(Constants.SearchEmployeeError);
         }
     }
 
@@ -181,7 +170,7 @@ public partial class EMS
         EmployeeFilters? filters = GetEmployeeFiltersFromConsole();
         if (filters == null)
         {
-            _logger.LogError("Error retrieving employee filters from console.");
+            _logger.LogError(Constants.GetEmployeeFiltersFromConsoleError);
             return;
         }
         try
@@ -189,12 +178,13 @@ public partial class EMS
             List<EmployeeDetails> employees = _employeeBal.FilterEmployees(filters);
             if (employees != null && employees.Count > 0)
             {
+                _logger.LogSuccess(Constants.FilterEmployeesSuccess);
                 PrintEmployeesDetails(employees);
             }
         }
-        catch (Exception ex)
+        catch (Exception)
         {
-            _logger.LogError($"Some error occur while filtering employees: {ex.Message}");
+            _logger.LogError(Constants.FilterEmployeesError);
         }
         ResetFilters(filters);
     }
@@ -202,7 +192,7 @@ public partial class EMS
     public static void CountEmployees()
     {
         int count = _employeeBal.CountEmployees();
-        _logger.LogSuccess($"Total Employees: {count}");
+        _logger.LogSuccess($"{Constants.CountEmployeesSuccess} {count}");
     }
 
     private static int HandleCommandArgs(string[] args, RootCommand rootCommand)
